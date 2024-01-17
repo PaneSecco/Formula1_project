a3e583e5eab17283d1064111ef9957b684da3627
Gatto lunatico <101564199+PaneSecco@users.noreply.github.com>
2024-01-17 19:43:44 +0100

    implementazione di tutte le funzionalità sui filtri

--- Formula1_project/Form1.Designer.cs
+++ Formula1_project/Form1.Designer.cs
@@ -33,13 +33,23 @@
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.Piloti = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.Team = new System.Windows.Forms.TabPage();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.Piloti.SuspendLayout();
@@ -50,11 +60,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(403, 31);
+            this.dataGridView1.Location = new System.Drawing.Point(236, 31);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(617, 418);
+            this.dataGridView1.Size = new System.Drawing.Size(784, 425);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
@@ -91,7 +101,11 @@
             // 
             // Piloti
             // 
-            this.Piloti.Controls.Add(this.button3);
+            this.Piloti.Controls.Add(this.label5);
+            this.Piloti.Controls.Add(this.radioButton5);
+            this.Piloti.Controls.Add(this.radioButton4);
+            this.Piloti.Controls.Add(this.label3);
+            this.Piloti.Controls.Add(this.textBox3);
             this.Piloti.Controls.Add(this.label1);
             this.Piloti.Controls.Add(this.textBox1);
             this.Piloti.Controls.Add(this.dataGridView1);
@@ -107,8 +121,49 @@
             this.Piloti.UseVisualStyleBackColor = true;
             this.Piloti.Click += new System.EventHandler(this.tabPage1_Click);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Filtro per team";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(29, 269);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(160, 22);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Filtro per cognome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 184);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Team
             // 
+            this.Team.Controls.Add(this.radioButton3);
+            this.Team.Controls.Add(this.radioButton2);
+            this.Team.Controls.Add(this.radioButton1);
+            this.Team.Controls.Add(this.label4);
+            this.Team.Controls.Add(this.label2);
+            this.Team.Controls.Add(this.textBox2);
             this.Team.Controls.Add(this.comboBox2);
             this.Team.Controls.Add(this.button2);
             this.Team.Controls.Add(this.dataGridView2);
@@ -121,6 +176,69 @@
             this.Team.Text = "Team";
             this.Team.UseVisualStyleBackColor = true;
             // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(22, 308);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 20);
+            this.radioButton3.TabIndex = 12;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Tutti";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(22, 282);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(70, 20);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ritirato";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(22, 256);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 20);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Attivo";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Attività";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Filtro per Nome";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(22, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(160, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
@@ -129,6 +247,7 @@
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(160, 24);
             this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // button2
             // 
@@ -144,41 +263,46 @@
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(400, 32);
+            this.dataGridView2.Location = new System.Drawing.Point(233, 32);
             this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
-            this.dataGridView2.Size = new System.Drawing.Size(620, 380);
+            this.dataGridView2.Size = new System.Drawing.Size(787, 417);
             this.dataGridView2.TabIndex = 4;
             this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
-            // textBox1
+            // radioButton4
             // 
-            this.textBox1.Location = new System.Drawing.Point(29, 173);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(160, 22);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(29, 361);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(88, 20);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Alfabetico";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
             // 
-            // label1
+            // radioButton5
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(26, 138);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Filtro per Cognome";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(29, 387);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(113, 20);
+            this.radioButton5.TabIndex = 10;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Di inserimento";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
             // 
-            // button3
+            // label5
             // 
-            this.button3.Location = new System.Drawing.Point(29, 229);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(161, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Ordine alfabetico";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ordine:";
             // 
             // Form1
             // 
@@ -195,6 +319,7 @@
             this.Piloti.ResumeLayout(false);
             this.Piloti.PerformLayout();
             this.Team.ResumeLayout(false);
+            this.Team.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
@@ -213,7 +338,17 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
     }
 }
 
--- Formula1_project/Form1.cs
+++ Formula1_project/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Formula1_project
@@ -19,6 +20,9 @@ namespace Formula1_project
             InitializeComponent();
 
             tabControl1.SelectedIndexChanged += tabControl1_SelectedIndexChanged;
+            //textBox4.KeyPress += textBox4_KeyPress;
+            //textBox4.TextChanged += textBox4_TextChanged;
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -54,38 +58,42 @@ namespace Formula1_project
             }
             comboBox1.Items.Add("Mostra tutto");
             comboBox1.SelectedIndex = NumeroIndici;
+
+            radioButton3.Checked = true;
+            radioButton5.Checked = true;
+
+
+            ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
+            conn = new MySqlConnection(ConnectionString);
+            conn.Open();
+
+            sql1 = "select * from team;";
+            cmd1 = new MySqlCommand(sql1, conn);
+
+            MyAdapter = new MySqlDataAdapter(cmd1);
+            dati = new DataTable();
+            MyAdapter.Fill(dati);
+
+            dataGridView2.DataSource = dati;
+
+            conn.Close();
+
+            //carico i filtri nella combobox2
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            NumeroIndici = 0;
+            foreach (DataGridViewColumn colonna in dataGridView2.Columns)
+            {
+                string nomeColonna = colonna.HeaderText;
+                comboBox2.Items.Add(nomeColonna);
+                NumeroIndici++;
+            }
+            comboBox2.Items.Add("Mostra tutto");
+            comboBox2.SelectedIndex = NumeroIndici;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Prendo il nome della colonna che voglio far vedere
-            string nomeColonnaDaMostrare = comboBox1.Text;
-
-            if(nomeColonnaDaMostrare!= "Mostra tutto")
-            {
-                // Nascondi tutte le colonne
-                foreach (DataGridViewColumn colonna in dataGridView1.Columns)
-                {
-                    colonna.Visible = false;
-                }
-
-                // Trova la colonna con il nome desiderato e la mostra
-                foreach (DataGridViewColumn colonna in dataGridView1.Columns)
-                {
-                    if (colonna.HeaderText == nomeColonnaDaMostrare)
-                    {
-                        colonna.Visible = true;
-                        break;  // Esco dopo aver trovato la colonna desiderata
-                    }
-                }
-            }
-            else
-            {
-                foreach (DataGridViewColumn colonna in dataGridView1.Columns)
-                {
-                    colonna.Visible = true;
-                }
-            }
+            text_changed1();
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -105,6 +113,8 @@ namespace Formula1_project
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
             int SchedaSelezionata = tabControl1.SelectedIndex;
 
             if (SchedaSelezionata == 0)
@@ -177,52 +187,12 @@ namespace Formula1_project
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // Prendo il nome della colonna che voglio far vedere
-            string nomeColonnaDaMostrare = comboBox2.Text;
-
-            if (nomeColonnaDaMostrare != "Mostra tutto")
-            {
-                // Nascondi tutte le colonne
-                foreach (DataGridViewColumn colonna in dataGridView2.Columns)
-                {
-                    colonna.Visible = false;
-                }
-
-                // Trova la colonna con il nome desiderato e la mostra
-                foreach (DataGridViewColumn colonna in dataGridView2.Columns)
-                {
-                    if (colonna.HeaderText == nomeColonnaDaMostrare)
-                    {
-                        colonna.Visible = true;
-                        break;  // Esco dopo aver trovato la colonna desiderata
-                    }
-                }
-            }
-            else
-            {
-                foreach (DataGridViewColumn colonna in dataGridView2.Columns)
-                {
-                    colonna.Visible = true;
-                }
-            }
+            text_changed2();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            String ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
-            MySqlConnection conn = new MySqlConnection(ConnectionString);
-            conn.Open();
-
-            String sql1 = "SELECT * FROM piloti WHERE Cognome LIKE '" + textBox1.Text + "%';";
-            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
-
-            MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1);
-            DataTable dati = new DataTable();
-            MyAdapter.Fill(dati);
-
-            dataGridView1.DataSource = dati;
-
-            conn.Close();
+            text_changed1();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -230,13 +200,89 @@ namespace Formula1_project
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            text_changed2();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            //filtro per team
+            text_changed1();
+
+        }
+
+        /*
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            //filtro per anno
+            // Se il testo è più lungo di 4 cifre, non fa nulla
+            if (textBox4.Text.Length > 10)
+            {
+                textBox4.Text = textBox4.Text.Substring(0, 10);
+                textBox4.SelectionStart = textBox4.Text.Length; // Imposta il cursore alla fine del testo
+            }
+
+            text_changed();
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != '/')
+            {
+                e.Handled = true; // Ignora il carattere
+            }
+        }
+
+        */
+
+        public void text_changed1()
         {
             String ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
             MySqlConnection conn = new MySqlConnection(ConnectionString);
             conn.Open();
 
-            String sql1 = "SELECT * FROM piloti ORDER BY Cognome;";
+            int num_sql = 0;
+
+            String sql1= "SELECT * FROM piloti ";
+
+            string nomeColonnaDaMostrare = comboBox1.Text;
+
+            if (nomeColonnaDaMostrare != "Mostra tutto" && nomeColonnaDaMostrare != "")
+            {
+                sql1 = "SELECT " + nomeColonnaDaMostrare + " FROM piloti ";
+            }
+
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                num_sql++;
+                sql1 = "SELECT * FROM piloti WHERE Cognome LIKE '" + textBox1.Text + "%'";
+            }
+
+            if (!string.IsNullOrEmpty(textBox3.Text))
+            {
+                num_sql++;
+                if(num_sql > 1)
+                {
+                    sql1 += "AND Team LIKE '" + textBox3.Text + "%'";
+                }
+                else
+                {
+                    sql1 = "SELECT * FROM piloti WHERE Team LIKE '" + textBox3.Text + "%'";
+                }
+            }
+
+            if (radioButton4.Checked == true)
+            {
+                sql1 += "ORDER BY Cognome";
+            }
+
+            sql1 += ";";
             MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
 
             MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1);
@@ -246,6 +292,92 @@ namespace Formula1_project
             dataGridView1.DataSource = dati;
 
             conn.Close();
+        }
+
+        public void text_changed2()
+        {
+            // Prendo il nome della colonna che voglio far vedere
+            string nomeColonnaDaMostrare = comboBox2.Text;
+
+            String ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+            conn.Open();
+
+            String sql1 = "select * from team ";
+
+            if (nomeColonnaDaMostrare != "Mostra tutto" && nomeColonnaDaMostrare!="")
+            {
+                sql1 = "select " + nomeColonnaDaMostrare + " from team ";
+            }
+
+            //MessageBox.Show(nomeColonnaDaMostrare);
+
+            int num_sql = 0;
+
+            if (radioButton1.Checked == true)
+            {
+                sql1 += "WHERE Attivita=1 ";
+                num_sql++;
+            }
+            
+            if(radioButton2.Checked == true)
+            {
+                sql1 += "WHERE Attivita=0 ";
+                num_sql++;
+            }
+
+            if (!string.IsNullOrEmpty(textBox2.Text))
+            {
+                if (num_sql > 0)
+                {
+                    sql1 += "AND Nome LIKE '" + textBox2.Text + "%' ";
+                }
+                else
+                {
+                    sql1 += "WHERE Nome LIKE '" + textBox2.Text + "%' ";
+                }
+            }
+
+            sql1 += ";";
+            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1);
+            DataTable dati = new DataTable();
+            MyAdapter.Fill(dati);
+
+            dataGridView2.DataSource = dati;
+
+            conn.Close();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            text_changed2();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            text_changed2();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            text_changed2();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            text_changed2();
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            text_changed2();
         }
     }
 }
