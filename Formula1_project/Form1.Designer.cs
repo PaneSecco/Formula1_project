1295cf18682e12917131fe325477d407062c64ac
Gatto lunatico <101564199+PaneSecco@users.noreply.github.com>
2024-01-17 11:54:51 +0100

    Aggiungere i file di progetto.

---
+++ Formula1_project/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Formula1_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Piloti = new System.Windows.Forms.TabPage();
+            this.Team = new System.Windows.Forms.TabPage();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.Piloti.SuspendLayout();
+            this.Team.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(403, 31);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(617, 418);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Filtra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(29, 40);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Piloti);
+            this.tabControl1.Controls.Add(this.Team);
+            this.tabControl1.Location = new System.Drawing.Point(0, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1067, 548);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // Piloti
+            // 
+            this.Piloti.Controls.Add(this.button3);
+            this.Piloti.Controls.Add(this.label1);
+            this.Piloti.Controls.Add(this.textBox1);
+            this.Piloti.Controls.Add(this.dataGridView1);
+            this.Piloti.Controls.Add(this.comboBox1);
+            this.Piloti.Controls.Add(this.button1);
+            this.Piloti.Location = new System.Drawing.Point(4, 25);
+            this.Piloti.Margin = new System.Windows.Forms.Padding(4);
+            this.Piloti.Name = "Piloti";
+            this.Piloti.Padding = new System.Windows.Forms.Padding(4);
+            this.Piloti.Size = new System.Drawing.Size(1059, 519);
+            this.Piloti.TabIndex = 0;
+            this.Piloti.Text = "Piloti";
+            this.Piloti.UseVisualStyleBackColor = true;
+            this.Piloti.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // Team
+            // 
+            this.Team.Controls.Add(this.comboBox2);
+            this.Team.Controls.Add(this.button2);
+            this.Team.Controls.Add(this.dataGridView2);
+            this.Team.Location = new System.Drawing.Point(4, 25);
+            this.Team.Margin = new System.Windows.Forms.Padding(4);
+            this.Team.Name = "Team";
+            this.Team.Padding = new System.Windows.Forms.Padding(4);
+            this.Team.Size = new System.Drawing.Size(1059, 519);
+            this.Team.TabIndex = 1;
+            this.Team.Text = "Team";
+            this.Team.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(22, 43);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 89);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Filtra";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(400, 32);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(620, 380);
+            this.dataGridView2.TabIndex = 4;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 173);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(160, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Filtro per Cognome";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Ordine alfabetico";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 549);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.Piloti.ResumeLayout(false);
+            this.Piloti.PerformLayout();
+            this.Team.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Team;
+        private System.Windows.Forms.TabPage Piloti;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Formula1_project/Form1.cs
@@ -0,0 +1,251 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Formula1_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            tabControl1.SelectedIndexChanged += tabControl1_SelectedIndexChanged;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //cambio la visualizzazione della gridview e della tabcontrol
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            tabControl1.SelectedIndex = 0;
+
+            //avvio la connessione
+            String ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+            conn.Open();
+
+            String sql1 = "select * from piloti;";
+            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1);
+            DataTable dati = new DataTable();
+            MyAdapter.Fill(dati);
+
+            dataGridView1.DataSource = dati;
+
+            conn.Close();
+
+            //carico i filtri nella combobox1
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            int NumeroIndici = 0;
+            foreach (DataGridViewColumn colonna in dataGridView1.Columns)
+            {
+                string nomeColonna = colonna.HeaderText;
+                comboBox1.Items.Add(nomeColonna);
+                NumeroIndici++;
+            }
+            comboBox1.Items.Add("Mostra tutto");
+            comboBox1.SelectedIndex = NumeroIndici;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Prendo il nome della colonna che voglio far vedere
+            string nomeColonnaDaMostrare = comboBox1.Text;
+
+            if(nomeColonnaDaMostrare!= "Mostra tutto")
+            {
+                // Nascondi tutte le colonne
+                foreach (DataGridViewColumn colonna in dataGridView1.Columns)
+                {
+                    colonna.Visible = false;
+                }
+
+                // Trova la colonna con il nome desiderato e la mostra
+                foreach (DataGridViewColumn colonna in dataGridView1.Columns)
+                {
+                    if (colonna.HeaderText == nomeColonnaDaMostrare)
+                    {
+                        colonna.Visible = true;
+                        break;  // Esco dopo aver trovato la colonna desiderata
+                    }
+                }
+            }
+            else
+            {
+                foreach (DataGridViewColumn colonna in dataGridView1.Columns)
+                {
+                    colonna.Visible = true;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {   
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int SchedaSelezionata = tabControl1.SelectedIndex;
+
+            if (SchedaSelezionata == 0)
+            {
+                //avvio la connessione
+                String ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
+                MySqlConnection conn = new MySqlConnection(ConnectionString);
+                conn.Open();
+
+                String sql1 = "select * from piloti;";
+                MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1);
+                DataTable dati = new DataTable();
+                MyAdapter.Fill(dati);
+
+                dataGridView1.DataSource = dati;
+
+                conn.Close();
+
+                //carico i filtri nella combobox1
+                comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+                int NumeroIndici = 0;
+                foreach (DataGridViewColumn colonna in dataGridView1.Columns)
+                {
+                    string nomeColonna = colonna.HeaderText;
+                    comboBox1.Items.Add(nomeColonna);
+                    NumeroIndici++;
+                }
+                comboBox1.Items.Add("Mostra tutto");
+                comboBox1.SelectedIndex = NumeroIndici;
+            }
+
+            if (SchedaSelezionata == 1)
+            {
+                //avvio la connessione
+                String ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
+                MySqlConnection conn = new MySqlConnection(ConnectionString);
+                conn.Open();
+
+                String sql1 = "select * from team;";
+                MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1);
+                DataTable dati = new DataTable();
+                MyAdapter.Fill(dati);
+
+                dataGridView2.DataSource = dati;
+
+                conn.Close();
+
+                //carico i filtri nella combobox2
+                comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+                int NumeroIndici = 0;
+                foreach (DataGridViewColumn colonna in dataGridView2.Columns)
+                {
+                    string nomeColonna = colonna.HeaderText;
+                    comboBox2.Items.Add(nomeColonna);
+                    NumeroIndici++;
+                }
+                comboBox2.Items.Add("Mostra tutto");
+                comboBox2.SelectedIndex = NumeroIndici;
+            }
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Prendo il nome della colonna che voglio far vedere
+            string nomeColonnaDaMostrare = comboBox2.Text;
+
+            if (nomeColonnaDaMostrare != "Mostra tutto")
+            {
+                // Nascondi tutte le colonne
+                foreach (DataGridViewColumn colonna in dataGridView2.Columns)
+                {
+                    colonna.Visible = false;
+                }
+
+                // Trova la colonna con il nome desiderato e la mostra
+                foreach (DataGridViewColumn colonna in dataGridView2.Columns)
+                {
+                    if (colonna.HeaderText == nomeColonnaDaMostrare)
+                    {
+                        colonna.Visible = true;
+                        break;  // Esco dopo aver trovato la colonna desiderata
+                    }
+                }
+            }
+            else
+            {
+                foreach (DataGridViewColumn colonna in dataGridView2.Columns)
+                {
+                    colonna.Visible = true;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+            conn.Open();
+
+            String sql1 = "SELECT * FROM piloti WHERE Cognome LIKE '" + textBox1.Text + "%';";
+            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1);
+            DataTable dati = new DataTable();
+            MyAdapter.Fill(dati);
+
+            dataGridView1.DataSource = dati;
+
+            conn.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String ConnectionString = "server=127.0.0.1;uid=RaoulBova;pwd=12345;database=formula1";
+            MySqlConnection conn = new MySqlConnection(ConnectionString);
+            conn.Open();
+
+            String sql1 = "SELECT * FROM piloti ORDER BY Cognome;";
+            MySqlCommand cmd1 = new MySqlCommand(sql1, conn);
+
+            MySqlDataAdapter MyAdapter = new MySqlDataAdapter(cmd1);
+            DataTable dati = new DataTable();
+            MyAdapter.Fill(dati);
+
+            dataGridView1.DataSource = dati;
+
+            conn.Close();
+        }
+    }
+}
